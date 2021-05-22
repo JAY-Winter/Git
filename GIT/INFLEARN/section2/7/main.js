9781ffab69ebaf430b877e34aaeeff4ecae10b92
Jay-Thomas-code <rubat0@naver.com>
2021-05-22 20:55:46 +0900

    section2

---
+++ GIT/INFLEARN/section2/7/main.js
@@ -0,0 +1,29 @@
+let arr =[
+    
+    [5,3,7,2,3],
+    [3,7,1,6,1],
+    [7,2,5,3,4],
+    [4,3,6,5,1],
+    [8,7,3,5,2],
+];
+
+function Highest(a){
+
+    let answer = 0;
+    let n = a.length;
+
+
+
+
+
+);
+
+
+7 = arr[1,1]
+left = arr[1,0]
+up = arr[0,1]
+right = arr[1,2]
+
+
+
+
